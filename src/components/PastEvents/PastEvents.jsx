4144ddc1ef223db7eb77027197eea4aa391edcca
AdulIT <spilberg.max@bk.ru>
2023-02-09 14:17:44 +0600

    feat&fix: timeline section animation fix

--- src/components/PastEvents/PastEvents.jsx
+++ src/components/PastEvents/PastEvents.jsx
@@ -1,6 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const PastEvents = () => {
+
+    useEffect(() =>
+    {
+        const items = document.querySelectorAll(".timeline-section li");
+  
+        function isElementInViewport(el) {
+        let rect = el.getBoundingClientRect();
+        if(window.innerWidth >= 560){
+            return(
+                rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <=
+            (window.innerHeight  || document.documentElement.clientHeight ) &&
+            rect.right <= (window.innerWidth  || document.documentElement.clientWidth )
+            );
+        }
+        else {
+            return (
+                true
+            );
+        }
+        }
+    
+        function slideIn() {
+        for (let i = 0; i < items.length; i++) {
+            if (isElementInViewport(items[i])) {
+            items[i].classList.add("slide-in");
+            } else {
+            items[i].classList.remove("slide-in");
+            }
+        }
+        }
+    
+        window.addEventListener("load", slideIn);
+        window.addEventListener("scroll", slideIn);
+        window.addEventListener("resize", slideIn);
+    }, [])
+
     return (
         <section id="past-events-section-id" className="past-events-section section">
             <div id="myHeader">
